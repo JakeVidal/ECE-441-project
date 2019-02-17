56b36113e820e48aa6c9c3957b0c60429ce5383f
JakeVidal <jrvidal@shaw.ca>
2019-02-16 16:47:30 -0800

    updated input driver code

--- input_driver.vhd
+++ input_driver.vhd
@@ -8,7 +8,19 @@ entity input_driver is
             y_input         : in STD_LOGIC;
             z_input         : in STD_LOGIC;
             cordic_mode     : in STD_LOGIC;
-
-
+            initial_x       : out STD_LOGIC_VECTOR (15 downto 0);
+            initial_y       : out STD_LOGIC_VECTOR (15 downto 0);
+            initial_z       : out STD_LOGIC_VECTOR (15 downto 0);
     );
 end input_driver;
+
+architecture behavioural of input_driver is
+type   state_type is (input, input_x, input_y, input_z, output);  
+signal state : state_type := input;  
+begin
+
+process (state)
+begin
+end process;
+
+end behavioural;
