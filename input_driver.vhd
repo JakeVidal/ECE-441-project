6fb0f28be37378ccc8d0fabeae167135736fdb56
JakeVidal <jrvidal@shaw.ca>
2019-02-19 13:42:00 -0800

    added state machine and output logic

--- input_driver.vhd
+++ input_driver.vhd
@@ -41,8 +41,7 @@ architecture behavioural of input_driver is
     signal x_input_debounced, y_input_debounced, z_input_debounced : STD_LOGIC;
     signal cordic_mode_debounced, start_cordic_debounced : STD_LOGIC;
     signal keypad_row_debounced : STD_LOGIC_VECTOR (3 downto 0);
-
-    signal decode_value : STD_LOGIC_VECTOR (3 downto 0)
+    signal x_input_done, y_input_done, z_input_done : STD_LOGIC;
 
     type   state_type is (input, input_x, input_y, input_z, output);  
     signal state : state_type := input;  
@@ -60,8 +59,34 @@ begin
         row_debouncer: debouncer port map (clk, reset, keypad_row(i), keypad_row_debounced(i));
     end generate generate_row_debouncer;
 
-    decode: process (state, reset) is
+    state_machine: process (clk, state, reset) is
     begin
+
+        if reset then
+            state := input;
+
+        case state is
+            when (input AND x_input) => state := input_x;
+            when (input AND y_input) => state := input_y; 
+            when (input AND z_input) => state := input_z;  
+            when (input_x AND NOT x_input) => state := input;
+            when (input_y AND NOT y_input) => state := input; 
+            when (input_z AND NOT z_input) => state := input;   
+            when (input AND x_input_done AND y_input_done AND z_input_done) => state := output; 
+        end case;
+    end process state_machine;
+
+    decode: process (clk, state, reset) is
+
+        signal decode_value : STD_LOGIC_VECTOR (3 downto 0)
+        signal x_iteration, y_iteration, z_iteration : STD_LOGIC_VECTOR (2 downto 0);
+    
+    begin
+
+        if reset then
+            x_iteration <= "00";
+            y_iteration <= "00";
+            z_iteration <= "00";
 
         keypad_col <= "0111";
         if keypad_row = "0111" then
@@ -107,10 +132,56 @@ begin
             decode_value <= "1101"; --D
         end if;
 
-    end process decode;
+        if (x_input AND NOT y_input AND NOT z_input) then
 
-    state_machine: process (state, reset) is
-    begin
-    end process state_machine;
+            if x_iteration = "00"
+                initial_x( 15 downto 12) <= decode_value;
+                x_iteration = x_iteration + "1"
+            elif x_iteration = "01"
+                initial_x( 11 downto 8) <= decode_value;
+                x_iteration = x_iteration + "1"
+            elif x_iteration = "10"
+                initial_x( 7 downto 4) <= decode_value;
+                x_iteration = x_iteration + "1"
+            elif x_iteration = "11"
+                initial_x( 3 downto 0) <= decode_value;
+                x_iteration <= "00"
+            end if;
+
+        elsif (y_input AND NOT x_input AND NOT z_input) then
+
+            if y_iteration = "00"
+                initial_y( 15 downto 12) <= decode_value;
+                y_iteration = y_iteration + "1"
+            elif y_iteration = "01"
+                initial_y( 11 downto 8) <= decode_value;
+                y_iteration = y_iteration + "1"
+            elif y_iteration = "10"
+                initial_y( 7 downto 4) <= decode_value;
+                y_iteration = y_iteration + "1"
+            elif y_iteration = "11"
+                initial_y( 3 downto 0) <= decode_value;
+                y_iteration <= "00"
+            end if;
+
+        elsif (z_input AND NOT x_input AND NOT y_input) then
+
+            if z_iteration = "00"
+                initial_z( 15 downto 12) <= decode_value;
+                z_iteration = z_iteration + "1"
+            elif z_iteration = "01"
+                initial_z( 11 downto 8) <= decode_value;
+                z_iteration = z_iteration + "1"
+            elif z_iteration = "10"
+                initial_z( 7 downto 4) <= decode_value;
+                z_iteration = z_iteration + "1"
+            elif z_iteration = "11"
+                initial_z( 3 downto 0) <= decode_value;
+                z_iteration <= "00"
+            end if;
+            
+        end if;
+
+    end process decode;
 
 end behavioural;
