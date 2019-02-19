fa84e87d93a4f45846fb15826f0825efc757bde2
JakeVidal <jrvidal@shaw.ca>
2019-02-18 20:03:37 -0800

    removed redundant code

--- input_driver.vhd
+++ input_driver.vhd
@@ -7,17 +7,17 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity input_driver is 
     Port (
-            clk             : in STD_LOGIC;
-            reset           : in STD_LOGIC;
+            clk                       : in STD_LOGIC;
+            reset                     : in STD_LOGIC;
 
-            x_input         : in STD_LOGIC;
-            y_input         : in STD_LOGIC;
-            z_input         : in STD_LOGIC;
-            cordic_mode     : in STD_LOGIC;
-            start_cordic    : in STD_LOGIC;
+            x_input                   : in STD_LOGIC;
+            y_input                   : in STD_LOGIC;
+            z_input                   : in STD_LOGIC;
+            cordic_mode               : in STD_LOGIC;
+            start_cordic              : in STD_LOGIC;
 
-            keypad_row      : in STD_LOGIC_VECTOR (3 downto 0);
-            keypad_col      : out STD_LOGIC_VECTOR (3 downto 0);
+            keypad_row                : in STD_LOGIC_VECTOR (3 downto 0);
+            keypad_col                : out STD_LOGIC_VECTOR (3 downto 0);
 
             initial_x                 : out STD_LOGIC_VECTOR (15 downto 0);
             initial_y                 : out STD_LOGIC_VECTOR (15 downto 0);
@@ -56,16 +56,11 @@ begin
     mode_debouncer: debouncer port map (clk, reset, cordic_mode, cordic_mode_debounced);
     start_debouncer: debouncer port map (clk, reset, start_cordic, start_cordic_debounced);
 
-    row0_debouncer: debouncer port map (clk, reset, keypad_row(0), keypad_row_debounced(0));
-    row1_debouncer: debouncer port map (clk, reset, keypad_row(1), keypad_row_debounced(1));
-    row2_debouncer: debouncer port map (clk, reset, keypad_row(2), keypad_row_debounced(2));
-    row3_debouncer: debouncer port map (clk, reset, keypad_row(3), keypad_row_debounced(3));
-
     generate_row_debouncer: for i in 0 to 3 generate
         row_debouncer: debouncer port map (clk, reset, keypad_row(i), keypad_row_debounced(i));
     end generate generate_row_debouncer;
 
-    decode: process (state) is
+    decode: process (state, reset) is
     begin
 
         keypad_col <= "0111";
@@ -114,7 +109,7 @@ begin
 
     end process decode;
 
-    state_machine: process (state) is
+    state_machine: process (state, reset) is
     begin
     end process state_machine;
 
