1cc6d3cbcb9edfd0736d107ace8073756aca9065
alexander_cote <alexcotework@gmail.com>
2019-03-10 13:13:27 -0700

    cleaned up code in cordic controller

--- ECE-441-VHDL-project/ECE-441-VHDL-project.srcs/sources_1/imports/Desktop/cordic.vhdl
+++ ECE-441-VHDL-project/ECE-441-VHDL-project.srcs/sources_1/imports/Desktop/cordic.vhdl
@@ -3,22 +3,23 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.numeric_std.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 entity CORDIC is
 	port (
-	clock                                 : in std_logic;
-	reset                                 : in std_logic;
-		
-	x_initial, y_initial, z_initial       : in signed(15 downto 0);
-	cordic_mode                           : in std_logic;
-	start                                 : in std_logic;
-		
-	x_result, y_result, z_result          : out signed(15 downto 0)
-	iteration                             : out unsigned ( 3 downto 0 ) := "0000";
-	iteration_complete                    : out std_logic
-	
-
+	        in_clock                : in STD_LOGIC;
+	        in_reset                : in STD_LOGIC;
+	        in_x_initial            : in STD_LOGIC_VECTOR ( 15 downto 0 );
+	        in_y_initial            : in STD_LOGIC_VECTOR ( 15 downto 0 );
+	        in_z_initial            : in STD_LOGIC_VECTOR ( 15 downto 0 );
+	        in_cordic_mode          : in STD_LOGIC;
+	        in_start                : in STD_LOGIC;
+	        
+	        out_x_result             : out STD_LOGIC_VECTOR ( 15 downto 0 );
+	        out_y_result             : out STD_LOGIC_VECTOR ( 15 downto 0 );
+	        out_z_result             : out STD_LOGIC_VECTOR ( 15 downto 0 );
+	        out_iteration            : out STD_LOGIC_VECTOR (  3 downto 0 );
+	        out_iteration_complete   : out STD_LOGIC
 		);
 end CORDIC;
 	
@@ -26,57 +27,79 @@ architecture behaviour of CORDIC is
 	
 	component cordic_alu is 
 		Port (
-			trigger							:	in	std_logic;
-			x_in, y_in, z_in, theta			:	in	signed ( 15 downto 0 );
-			i								:	in 	unsigned ( 3 downto 0 );
-			mu								:	in	boolean;
-			x_out, y_out, z_out				:	out	signed ( 15 downto 0 );
-			done							:	out std_logic
+            trigger				: in STD_LOGIC;
+            x_in	  	        : in STD_LOGIC_VECTOR ( 15 downto 0 );
+            y_in			    : in STD_LOGIC_VECTOR ( 15 downto 0 );
+            z_in		        : in STD_LOGIC_VECTOR ( 15 downto 0 );
+            theta		     	: in STD_LOGIC_VECTOR ( 15 downto 0 );			
+            i					: in STD_LOGIC_VECTOR (  3 downto 0 );
+            mu					: in STD_LOGIC;
+            x_out				: out STD_LOGIC_VECTOR ( 15 downto 0 );
+            y_out				: out STD_LOGIC_VECTOR ( 15 downto 0 );
+            z_out				: out STD_LOGIC_VECTOR ( 15 downto 0 );
+            done				: out STD_LOGIC
 		);
 	end component;
 	
 	
-	signal setup_alu                                : std_logic := "0";
-	signal alu_trigger                              : std_logic := "0";
-	signal alu_x_input, alu_y_input, alu_z_input    : signed(15 downto 0);
-	signal alu_mu                                   : boolean := "0";
-	signal alu_completed                            : std_logic := "0";
+	signal iteration       : STD_LOGIC_VECTOR (  3 downto 0 );
+    signal x_current       : STD_LOGIC_VECTOR ( 15 downto 0 );
+    signal y_current       : STD_LOGIC_VECTOR ( 15 downto 0 );
+    signal z_current       : STD_LOGIC_VECTOR ( 15 downto 0 );
+	
+	signal setup_alu       : STD_LOGIC;
+	signal alu_trigger     : STD_LOGIC;
+	signal alu_x_input     : STD_LOGIC_VECTOR ( 15 downto 0 );
+	signal alu_y_input     : STD_LOGIC_VECTOR ( 15 downto 0 );
+	signal alu_z_input     : STD_LOGIC_VECTOR ( 15 downto 0 );
+	signal theta           : STD_LOGIC_VECTOR ( 15 downto 0 );
+	signal alu_mu          : STD_LOGIC;
+	signal alu_completed   : STD_LOGIC;
 	
 begin
 	
-	c_alu: cordic_alu port map ( alu_trigger, 
-								 alu_x_input, alu_y_input, alu_z_input, 
-								 iteration, 
-								 alu_mu, 
-								 x_result, y_result, z_result,
-								 alu_completed
-								 );
+	c_alu: cordic_alu port map ( 
+	    trigger     => alu_trigger ,
+	    x_in	    => alu_x_input ,
+	    y_in	    => alu_y_input ,
+	    z_in	    => alu_z_input ,
+	    theta	    => theta       ,
+	    i		 	=> iteration   ,
+	    mu		    => alu_mu      ,
+	    -- reset       => in_reset    ,
+	    x_out	    => out_x_result,
+	    y_out	    => out_y_result,
+	    z_out	    => out_z_result,
+	    done	    => alu_completed
+		);
 						
 	
-	cordic: process (clock, reset) is
+	cordic: process (in_clock, in_reset) is
 	begin
-		if rising_edge(reset) then
-		iteration_complete <= 0;
-		x_current <= 0;
-		y_current <= 0;
-		z_current <= 0;
-		x_result <= 0;
-		y_result <= 0;
-		z_result <= 0;
+		if rising_edge(in_reset) then
+            out_iteration_complete <= '0';
+            x_current          <= (others => '0');
+            y_current          <= (others => '0');
+            z_current          <= (others => '0');
+            out_x_result       <= (others => '0');
+            out_y_result       <= (others => '0');
+            out_z_result       <= (others => '0');
 		
-		elsif rising_edge(clock) then
+		elsif rising_edge(in_clock) then
 		
-		determine mode
+		--determine mode
 		
-			if(start and iteration = 0)
-				setup alu
-				 -> set initial values to alu inputs
-				 -> find value of mu
-				 -> get value of theta
-				 
+			if(in_start = '1' and iteration = "0000") then
+			    out_iteration_complete <= '0';
+				--setup alu
+				--> set initial values to alu inputs
+				--> find value of mu
+				--> get value of theta
+				
+			end if; -- end (start and iteration = 0)
 				 
 			
-			
+		end if; -- end rising edge of clock or reset
 			
 
 				
