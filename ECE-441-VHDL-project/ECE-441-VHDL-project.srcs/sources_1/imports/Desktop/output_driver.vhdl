f1f17ead8ab6f5c98b2f274441cf3d47fb3189b5
JakeVidal <jrvidal@shaw.ca>
2019-03-26 15:38:45 -0700

    initial output driver formatting

--- ECE-441-VHDL-project/ECE-441-VHDL-project.srcs/sources_1/imports/Desktop/output_driver.vhdl
+++ ECE-441-VHDL-project/ECE-441-VHDL-project.srcs/sources_1/imports/Desktop/output_driver.vhdl
@@ -6,21 +6,21 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity output_driver is 
-    Port (
+    port (
+            ----------------------INPUTS-----------------------------------------------
             clk                       : in STD_LOGIC;
             reset                     : in STD_LOGIC;
-
             x_result                  : in SIGNED (15 downto 0);
             y_result                  : in SIGNED (15 downto 0);
             z_result                  : in SIGNED (15 downto 0);
             iteration                 : in UNSIGNED (3 downto 0);
             data_ready                : in STD_LOGIC;
-
             x_select                  : in STD_LOGIC;
             y_select                  : in STD_LOGIC;
             z_select                  : in STD_LOGIC;
             iteration_select          : in STD_LOGIC_VECTOR (3 downto 0);
 
+            ----------------------OUTPUTS----------------------------------------------
             anode                     : out STD_LOGIC_VECTOR (3 downto 0);
             segment                   : out STD_LOGIC_VECTOR (6 downto 0)
     );
@@ -29,51 +29,54 @@ end output_driver;
 architecture behavioural of output_driver is
 
 	component Result_dist_mem_gen is
-        Port (
-            a : in STD_LOGIC_VECTOR(3 DOWNTO 0);
-            d : in STD_LOGIC_VECTOR(15 DOWNTO 0);
-            clk : in STD_LOGIC;
-            we : in STD_LOGIC;
-            spo : out STD_LOGIC_VECTOR(15 DOWNTO 0)
+        port (
+            a                         : in STD_LOGIC_VECTOR(3 DOWNTO 0);
+            d                         : in STD_LOGIC_VECTOR(15 DOWNTO 0);
+            clk                       : in STD_LOGIC;
+            we                        : in STD_LOGIC;
+            spo                       : out STD_LOGIC_VECTOR(15 DOWNTO 0)
         );
     end component;
     
     component debouncer is 
-        Port ( 
-            clk_100MHz    : in  STD_LOGIC;
-            reset         : in  STD_LOGIC;
-            PB_in         : in STD_LOGIC;
-            PB_out        : out STD_LOGIC    
+        port ( 
+            clk_100MHz                : in  STD_LOGIC;
+            reset                     : in  STD_LOGIC;
+            PB_in                     : in STD_LOGIC;
+            PB_out                    : out STD_LOGIC    
         );
     end component;
 
     component hex_driver is
-        Port (
-            clk           : in STD_LOGIC;
-            reset         : in STD_LOGIC;
-            done          : in STD_LOGIC;
-            d_in          : in STD_LOGIC_VECTOR (15 downto 0);
-            anodes        : out STD_LOGIC_VECTOR (3 downto 0);
-            cathodes      : out STD_LOGIC_VECTOR (6 downto 0)
+        port (
+            clk                       : in STD_LOGIC;
+            reset                     : in STD_LOGIC;
+            done                      : in STD_LOGIC;
+            d_in                      : in STD_LOGIC_VECTOR (15 downto 0);
+            anodes                    : out STD_LOGIC_VECTOR (3 downto 0);
+            cathodes                  : out STD_LOGIC_VECTOR (6 downto 0)
         );
     end component;
 
+    ------------------------MEMORY ACCESS SIGNALS----------------------------------
     signal write_enable: STD_LOGIC := '0';
     signal store_value: STD_LOGIC_VECTOR (15 downto 0);
     signal ram_address: STD_LOGIC_VECTOR (3 downto 0);
     signal x_stored_value, y_stored_value, z_stored_value: STD_LOGIC_VECTOR (15 downto 0);
-
+    
+    -------------------DEBOUNCED INPUT SIGNALS-------------------------------------
     signal x_select_debounced, y_select_debounced, z_select_debounced : STD_LOGIC;
     signal iteration_select_debounced : STD_LOGIC_VECTOR (3 downto 0);
-
+    
+    ---------------------------INTERNAL SIGNALS------------------------------------
     signal start_display : STD_LOGIC := '0';
     signal selected_value : STD_LOGIC_VECTOR (15 downto 0) := x"0000";
-    
     type   state_type is (mode_write, mode_read);  
     signal state : state_type := mode_write; 
 
 begin
 
+    -------------------------------MEMORY PORT MAPS--------------------------------
 	x_result_storage: Result_dist_mem_gen port map (
     a               =>   ram_address,
     d               =>   STD_LOGIC_VECTOR(x_result),
@@ -98,14 +101,15 @@ begin
     spo             =>   z_stored_value
     ); 
     
+    -------------------------------DEBOUNCER MAPS----------------------------------
     x_select_debouncer: debouncer port map (clk_100MHz => clk, reset => reset, PB_in => x_select, PB_out => x_select_debounced);
     y_select_debouncer: debouncer port map (clk_100MHz => clk, reset => reset, PB_in => y_select, PB_out => y_select_debounced);
     z_select_debouncer: debouncer port map (clk_100MHz => clk, reset => reset, PB_in => z_select, PB_out => z_select_debounced);
-
     generate_iteration_debouncer: for i in 0 to 3 generate
         iteration_debouncer: debouncer port map (clk_100MHz => clk, reset => reset, PB_in => iteration_select(i), PB_out => iteration_select_debounced(i));
     end generate generate_iteration_debouncer;
 
+    ------------------------------HEX DRIVER PORT MAP------------------------------
     display_value: hex_driver port map (clk => clk, reset => reset, done => start_display, d_in => selected_value, anodes => anode, cathodes => segment);
 
     handle_ui: process (clk, x_select_debounced, y_select_debounced, z_select_debounced, iteration_select_debounced, reset)
